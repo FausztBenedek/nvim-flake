5cddba52e9f065a876f54be94d2bbf889b00daa7
FausztBenedek <benedek.fauszt@fcbup.de>
2026-06-05 19:19:28 +0200

    Fix meta-l

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -80,6 +80,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+vim.keymap.set("n", "<D-l>", "<C-w>l", { noremap = true, silent = true })
 
 vim.keymap.set("t", "<C-w>", "<C-\\><C-n>", { noremap = true, silent = true })
 vim.keymap.set("t", "<C-n>", "<C-\\><C-n>:tabn<cr>", { noremap = true, silent = true })
